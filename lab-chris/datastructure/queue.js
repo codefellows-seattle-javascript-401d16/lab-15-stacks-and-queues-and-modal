f9cb96f9bd8642b696629e6cdaa09e65369aa49d
janousek77 <janousek77@gmail.com>
2017-07-01 21:24:03 -0700

    added all css stuff and stack and queue starting work on tests

---
+++ lab-chris/datastructure/queue.js
@@ -0,0 +1,12 @@
+'use strict'
+
+class Queue {
+  enqueue(){
+    Array.prototype.unshift.apply(this, arguments)
+    return this
+  }
+
+  dequeue(){
+    return Array.prototype.pop.call(this)
+  }
+}
