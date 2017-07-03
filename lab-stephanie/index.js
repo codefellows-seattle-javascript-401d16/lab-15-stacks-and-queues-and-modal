b92698e50cec1cf2661e53165274ce33f133b414
Stephanie Dover <doverstephaniem@gmail.com>
2017-07-02 23:11:39 -0700

    .

--- lab-stephanie/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-require('dotenv').config('.env');
-require('./lib/server.js').start();
--- lab-stephanie/test/lib/clear-db.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const Album = require('../../model/album.js');
-
-module.exports = () => {
-  return Promise.all([Album.remove({})]);
-};
