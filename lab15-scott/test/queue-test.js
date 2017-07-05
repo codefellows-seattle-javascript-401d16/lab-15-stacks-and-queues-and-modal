fe2968ca2db0bd9ca39ae85879f4b23f3a8a3384
Scott <sdmccoy@gmail.com>
2017-07-04 20:53:00 -0700

    the enqueue method test is passing

--- lab15-scott/test/queue-test.js
+++ lab15-scott/test/queue-test.js
@@ -5,9 +5,10 @@ const expect = require('expect');
 
 describe('Testing the QUEUE constructor', () => {
   describe('If putting something on to the queue', () => {
-    it('it should return with a combined array', () => {
-      Queue.enqueue(5);
-      expect(new Queue()[0]).toEqual(5);
+    it('it should return with a 5 at the beginning of the array', () => {
+      let newQueue = new Queue();
+      newQueue.enqueue(5);
+      expect(newQueue[0]).toEqual(5);
     })
   });
 });
