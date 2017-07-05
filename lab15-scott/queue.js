62a13f4c44489c17c003b9ff242ca1e43652d57f
Scott <sdmccoy@gmail.com>
2017-07-04 20:18:48 -0700

    created the queue class with two methods, enqueue and dequeue, on it

---
+++ lab15-scott/queue.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//create queue class with methods on it
+class Queue(){
+  enqueue(){
+    //create a new prototype on the Array constructor
+    //use the unshift method to add arguments to the beginning of the Array
+    //use apply that takes in a given this value and and array of arguments for the unshift method
+    //it should apply the arguments to the beginning/front of the array prototype
+    Array.prototype.unshift.apply(this, arguments);
+    //return the combined array
+    return this;
+  };
+
+  dequeue(){
+    //return the prototype array with and invoke the pop method on it
+    //use the call function since you don't need to attaach an array of arguments to it
+    //it will take in THIS and remove the last element and pop it off of the Queue
+    Array.prototype.pop.call(this);
+  };
+};
